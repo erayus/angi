4fb7795303c8a9af53589f5b8f91843998fb6846
erayus <raymondt@pageuppeople.com>
2022-04-27 03:04:58 +1000

    WIP Menu Creation

--- website/src/components/food-add-options-modal/food-add-options-modal.tsx
+++ website/src/components/food-add-options-modal/food-add-options-modal.tsx
@@ -7,28 +7,28 @@ const FoodAddOptionsModal = (props: Props) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     const onOptionChosen = () => { }
-}
-return (
-    <>
-        <Button onClick={onOpen}>Open Modal</Button>
 
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-                <ModalHeader>Food Add Options</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                        Add Food Manually
-                    </Button>
-                    {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            Add Food From Others
-                        </Button> */}
-                </ModalBody>
-            </ModalContent>
-        </Modal>
-    </>
-)
+    return (
+        <>
+            <Button onClick={onOpen}>Open Modal</Button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Food Add Options</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Add Food Manually
+                        </Button>
+                        {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Add Food From Others
+                            </Button> */}
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+    )
 }
 
 export default FoodAddOptionsModal
--- website/src/views/menu-create/menu-create.tsx
+++ website/src/views/menu-create/menu-create.tsx
@@ -1,18 +1,45 @@
-import { Box, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, Grid, Input, GridItem, Flex } from '@chakra-ui/react';
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Redirect } from 'react-router-dom'
+import FoodAdd from '../../components/food-add/food-add';
 import useAddMenu from '../../hooks/menu/useAddMenu'
 import useMenu from '../../hooks/menu/useMenu'
 import { Food, IUserFoodCategoryQuantity } from '../../models/Food'
 import { NavPath } from '../../utils/nav-path'
+import FoodManage from '../food-manage/food-manage';
 
 type Props = {}
 type MenuCreationFormValues = {
-    food_: Food[]
     renewPeriod_: number
-    foodCategoryQuanty_: IUserFoodCategoryQuantity[]
-    checkedIngredientIds_: string[]
+    food_: Food[],
+    entreeQuantity_: number,
+    mainQuantity_: number,
+    soupQuantity_: number,
+    dessertQuantity_: number,
+}
+
+const categoryQuantitiesFormData:any = {
+    entree: {
+        label: 'Entree',
+        inputId: 'entree-quantity',
+        formValue: 'entreeQuantity_'
+    },
+    main: {
+        label: 'Main',
+        inputId: 'main-quantity',
+        formValue: 'mainQuantity_'
+    },
+    soup: {
+        label: 'Soup',
+        inputId: 'soup-quantity',
+        formValue: 'soupQuantity_'
+    },
+    dessert: {
+        label: 'Dessert',
+        inputId: 'dessert-quantity',
+        formValue: 'dessertQuantity_'
+    },
 }
 
 const MenuCreate = (props: Props) => {
@@ -23,12 +50,34 @@ const MenuCreate = (props: Props) => {
     const onMenuCreationSubmitted = () => {
 
     }
+
     const menuCreationForm = (
         <form onSubmit={handleSubmit(onMenuCreationSubmitted)}>
             <FormControl isRequired mt={3}>
-                <FormLabel htmlFor='food-name'>Food Name</FormLabel>
-                <Input id='food-name' placeholder='Example Food' {...register("renewPeriod_")} />
+                <FormLabel htmlFor='renew-period'>Renew Period</FormLabel>
+                <Input type="number" id='renew-period' placeholder='Renew Period' {...register("renewPeriod_")} />
             </FormControl>
+
+            <FormLabel mt={3}>Category Quantity</FormLabel>
+            <FormControl as={Flex} alignItems="center" justifyContent="space-between" isRequired mt={3}>
+                <Box flex={2}>Entree:</Box>
+                <Input flex={2} type="number" id='entree-quantity' placeholder='1' {...register("entreeQuantity_")} />
+            </FormControl>
+            <FormControl as={Flex} alignItems="center" justifyContent="space-between" isRequired mt={3}>
+                <Box flex={2}>Main:</Box>
+                <Input flex={2} type="number" id='main-quantity' placeholder='1' {...register("mainQuantity_")} />
+            </FormControl>
+            <FormControl as={Flex} alignItems="center" justifyContent="space-between" isRequired mt={3}>
+                <Box flex={2}>Soup:</Box>
+                <Input flex={2} type="number" id='soup-quantity' placeholder='1' {...register("soupQuantity_")} />
+            </FormControl>
+            <FormControl as={Flex} alignItems="center" justifyContent="space-between" isRequired mt={3}>
+                <Box flex={2}>Dessert:</Box>
+                <Input flex={2} type="number" id='dessert-quantity' placeholder='1' {...register("dessertQuantity_")} />
+            </FormControl>
+
+            <FormLabel mt={3}>Food In Storage:</FormLabel>
+            <FoodManage/>
         </form>
     )
 
